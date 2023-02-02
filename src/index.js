ac4348f5e32bc39ac951fe8e26fe04c10d4e49ce
Bruno Costa <bcostinha01@gmail.com>
2023-02-02 16:23:51 +0000

    Setup radar-scope-js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import Chart from "chart.js";
+
+function createRadarChart(data, options) {
+  const chartId = `radar-chart-${Math.random()
+    .toString(36)
+    .substring(2, 15)}`;
+  const chartContainer = document.getElementById("div");
+  chartContainer.innerHTML = `
+        <canvas id="${chartId}"></canvas>
+    `;
+  const chartCanvas = chartContainer.querySelector(`#${chartId}`);
+  const chart = new Chart(chartCanvas, {
+    type: "radar",
+    data: data,
+    options: options,
+  });
+  return chartContainer;
+}
+
+export { createRadarChart };
